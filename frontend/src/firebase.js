e2536b26292ce398378bfb0067c085c2a56b6a14
apache-777-apache <tashfeenhaider777@gmail.com>
2026-04-20 02:42:38 +0500

    tt

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
 
 
 // TODO: Replace the following with your app's Firebase project configuration
@@ -19,3 +20,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app); 
+const analytics = getAnalytics(app);
